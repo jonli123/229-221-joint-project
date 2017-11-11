b2ccfa4f79d63692f66fa639ad1484cd9b20d9d0
Stephanie Dong <stephaniexd11@gmail.com>
2017-11-10 18:00:43 -0800

    Added shuffling to data

--- getUsers.py
+++ getUsers.py
@@ -1,10 +1,7 @@
 import csv
 import numpy as np
 import time
-
-
-
-
+import random
 
 #loads data in user by user, returns matrix of user data 56x51x71
 def loadData(filename):
@@ -21,25 +18,48 @@ def loadData(filename):
 
 	for i in data:
 		for j in data:
-			pairData.append((i[1:n],j[1:n]))
+			pairData.append(i[1:n]+j[1:n])
 			if j[0] == i[0]:
 				labels.append(1)
 			else:
 				labels.append(0)
-	print len(labels)
-	print len(pairData)
-	return np.array(users), np.array(pairData), np.array(labels)
 
+	labeledData = zip(pairData,labels)
+	#labeledData = pairData + labels
+	random.shuffle(labeledData)
+	cutoff = int(len(labeledData)*0.7)
+
+	#print cutoff
+	#print labeledData[0]
+
+	trainX, trainY = zip(*labeledData[:cutoff])
+	#print trainY
+	testX,testY= zip(*labeledData[cutoff:])
+
+	#np.savetxt('trainX.csv',trainX,delimiter = ',')
+	#np.savetxt('trainY.csv',trainY,delimiter = ',')
+	#np.savetxt('testX.csv',testX,delimiter = ',')
+	#np.savetxt('testY.csv',testY,delimiter = ',')
+
+	return np.array(trainX), np.array(trainY), np.array(testX), np.array(testY)
+
+def retreiveData():
+	trainX = np.genfromtxt('trainX.csv',delimiter=",",skip_header=False)
+	trainY = np.genfromtxt('trainY.csv',delimiter=",",skip_header=False)
+	testX = np.genfromtxt('testX.csv',delimiter=",",skip_header=False)
+	testY = np.genfromtxt('testY.csv',delimiter=",",skip_header=False)
+	return trainX, trainY, testX, testY
 
 def main():
 	start = time.time()
 
 	filename = 'keystroke.csv'
-	userMatrix,pairData,labels = loadData(filename)
-
+	trainX, trainY, testX, testY = loadData(filename)
+	#print len(trainData)
+	#print len(testData)
 	end = time.time()
 	print "Time to run:" + str(end-start)
 
 if __name__ == '__main__':
-    main()
+	main()
     
