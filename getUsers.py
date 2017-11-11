93326ab96687b5de2eec4fbd98c6e5497b2f3de0
johnnyli <johnnyli@stanford.edu>
2017-11-11 00:30:33 +0000

    updated getUsers

--- getUsers.py
+++ getUsers.py
@@ -1,19 +1,24 @@
 import csv
 import numpy as np
 
-filename = 'keystroke.csv'
-data = np.loadtxt(open(filename,'rb'),delimiter=',', skiprows=1)	
-m,n = data.shape
-users = [[] for y in range(56)]
-#print len(users)
-#print len(users[0])
 
-for i in range(m):
-	userNum = int(data[i][0])-1
-	##print userNum
-	users[userNum].append(data[i][1:n])
+#loads data in user by user, returns matrix of user data 56x51x71
+def loadData(filename):
+	data = np.loadtxt(open(filename,'rb'),delimiter=',', skiprows=1)	
+	m,n = data.shape
+	#print (m,n)
+	users = [[] for y in range(56)]
+	for i in range(m):
+		userNum = int(data[i][0])-1
+		users[userNum].append(data[i][1:n])
+	#print len(users)
+	#print len(users[0])
+	#print (users[0][0])
+	return users
 
-print len(users)
-print len(users[0])
-print len(users[5])
-print len(users[55])
+def main():
+	filename = 'keystroke.csv'
+	users = loadData(filename)
+
+if __name__ == '__main__':
+    main()
