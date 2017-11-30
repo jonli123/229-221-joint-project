e026315d74dc5757eb103dfe6390cdccd586be79
Stephanie Dong <stephaniexd11@gmail.com>
2017-11-29 19:52:02 -0800

    added tiling of attacks

--- KmeansAttach.py
+++ KmeansAttach.py
@@ -7,26 +7,24 @@ def compute_k_means(data, K=5):
     kmeans = KMeans(n_clusters=K, random_state=0).fit(data)
     return kmeans.labels_, kmeans.cluster_centers_
 
-def compute_covariance(cluster_assignments, cluster_centers, data):
+def get_data_clusters(cluster_assignments, cluster_centers, data):
     k = cluster_centers.shape[0]
     m = data.shape[0]
-    covs = []
-    for i in range(k):
-        cluster_points = np.asarray([data[j] for j in range(m) if cluster_assignments[j] == i])
-        covariance = np.cov(cluster_points.T)
-        covs.append(covariance)
-    return covs
+    return [np.asarray([data[j] for j in range(m) if cluster_assignments[j] == i]) for i in range(k)]
 
 
-def generate_multivariate_gaussian_attack(means, covs, counts, count_threshold=5):
+
+def generate_multivariate_gaussian_attack(cluster_assignments, cluster_centers, data, counts, count_threshold=5, tile_fn=None):
     '''
     Generate one attack per mean using multivariate gaussian
     '''
+    k = cluster_centers.shape[0]
+    clusters = get_data_clusters(cluster_assignments, cluster_centers, data)
     attacks = []
-    for i in range(means.shape[0]):
+    for i in range(k):
         if counts[i] >= count_threshold:
-            mean = means[i]
-            cov = covs[i]
+            mean = cluster_centers[i]
+            cov = np.cov(clusters[i].T)
 
             # Add small epsilon to cov is cov is too small
             min_eig = np.min(np.real(np.linalg.eigvals(cov)))
@@ -34,22 +32,34 @@ def generate_multivariate_gaussian_attack(means, covs, counts, count_threshold=5
                 cov -= 10 * min_eig * np.eye(*cov.shape)
 
             attack = np.random.multivariate_normal(mean, cov)
-            attacks.append(attack)
-    return attacks
+
+            if tile_fn:
+                attack = tile_fn(clusters[i], [attack])
+
+            attacks.extend(attack)
+    return np.asarray(attacks)
 
 
-# def tile_attacks(data, attacks):
-#     #tile each mean and data
+def tile_attacks(data, attacks):
+    n = data.shape[1]
+
+    tiled_attacks = []
+    for d in data:
+        for a in attacks:
+            example = np.concatenate((d, a))
+            tiled_attacks.append(example)
+
+    return np.asarray(tiled_attacks)
 
 
 def main():
     filename = 'keystroke.csv'
     labels, data = loadSingleData(filename)
     cluster_assignments, cluster_centers = compute_k_means(data, K=5)
-    covs = compute_covariance(cluster_assignments, cluster_centers, data)
     counts = [np.sum(cluster_assignments == i) for i in range(cluster_centers.shape[0])]
-    attacks = generate_multivariate_gaussian_attack(cluster_centers, covs, counts)
-    print(attacks)
+    attacks = generate_multivariate_gaussian_attack(cluster_assignments, cluster_centers, data, counts, tile_fn=tile_attacks)
+
+    print(attacks.shape)
 
 
 
